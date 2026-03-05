b1c9ea5cf7340082f196b297c5c0ebd421173d96
jenscranner <jenscrannerdahle@gmail.com>
2026-03-05 14:55:31 +0100

    asdsadasdasd

---
+++ Card.java
@@ -0,0 +1,20 @@
+package Poker-Emulasjon;
+
+public class Card {
+    private int value;
+    private char suit;
+    private String cardString;
+    private boolean visible;
+
+    public Card (int value, char suit, boolean visible) {
+        this.value = value;
+        this.suit = suit;
+        this.cardString = "" + suit + value;
+    }
+
+    public boolean getVisibility {return visible;}
+
+    public void setInvisble {visible = false;}
+
+    public void setVisible {visible = true;}
+}
---
+++ Deck.java
@@ -0,0 +1,21 @@
+
+package Poker-Emulasjon;
+
+include java.util.Array;
+include java.util.List;
+include java.util.Math;
+
+
+public class Deck { //Always 52 cards in a deck
+
+    private Array<List> = new
+
+    public Deck () {
+            char[] suits = ['h', 'd', 'c', 's'] //Heart, diamonds, clubs, spades
+            for (i = 1, i < 14, i++ ) {
+                for ()
+            }
+
+
+    }
+}
