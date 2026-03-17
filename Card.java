9ada1e3f1323b19b271d3489a10efa1ab0403cb1
jenscranner <jenscrannerdahle@gmail.com>
2026-03-17 19:49:59 +0100

    Updated functionality and debugging

--- Card.java
+++ Card.java
@@ -5,12 +5,19 @@ public class Card {
     private char suit;
     private String cardString;
     private boolean visible;
+    private int score;
 
     public Card (int value, char suit, boolean visible) {
         this.value = value;
         this.suit = suit;
         this.cardString = "" + suit + value;
         this.visible = visible;
+        if (value >= 10) {
+            this.score = 10;
+        } else {
+            this.score = value;
+        }
+
     }
 
     public boolean getVisibility() {return visible;}
@@ -19,6 +26,14 @@ public class Card {
 
     public void setVisible() {visible = true;}
 
-    public int getValue() {return value;}
+    public int getValue() {return score;}
+    //This returns the score that should be used for calculating the final score
+    //For suit and face use cardString
+    public String getCardString() {return cardString;}
+
+    public int returnAce() {
+        if (value == 1) {return 10;}
+        else {return 0;}
+    }
 
 }
